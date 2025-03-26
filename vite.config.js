798ce77ba5faa00372d70f9c3b99a3ca22e10ef7
Juan Jaramillo <juan@galileocapital.io>
2025-03-26 13:01:51 -0500

    feat: update imports

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import replaceImageUrl from 'vite-plugin-replace-image-url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    replaceImageUrl({
-      publicPath: process.env.VITE_APP_BASE_PATH
-    }),
+    react()
   ],
 })
